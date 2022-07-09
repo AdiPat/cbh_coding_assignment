407bf072b7f43b2d2a36d8b9bc83c191ddfb19b8
Aditya Patange <adipat247360@gmail.com>
2022-07-09 18:13:52 +0530

    dpk: Add tests

--- dpk.test.js
+++ dpk.test.js
@@ -5,4 +5,32 @@ describe("deterministicPartitionKey", () => {
     const trivialKey = deterministicPartitionKey();
     expect(trivialKey).toBe("0");
   });
+
+  it("Returns a hash when event has no partition key but other field. ", () => {
+    const event = {
+      key: "value"
+    };
+    const trivialKey = deterministicPartitionKey(event);
+    expect(typeof trivialKey).toEqual("string");
+    expect(trivialKey?.length).toBeGreaterThan(10);
+  });
+
+  it("Returns partition key if provided in the event. ", () => {
+    const event = {
+      partitionKey: "1234567890"
+    };
+    const trivialKey = deterministicPartitionKey(event);
+    expect(trivialKey).toEqual(event.partitionKey);
+  });
+
+  it("Returns partition key as string if provided in the event. ", () => {
+    const event = {
+      partitionKey: 123467890,
+    };
+    const trivialKey = deterministicPartitionKey(event);
+    expect(trivialKey).toStrictEqual(String(event.partitionKey));
+  });
+
+
+
 });
